3bc178d2b07db6f454ad864b7e6e561ee459bc6d
dinkiebinkie <b@binkie.town>
2019-06-07 08:46:19 -0400

    oops forgot to add words js

--- frontend/src/data/words.js
+++ frontend/src/data/words.js
@@ -1,3 +1,32 @@
-const words = [{}];
+const words = {
+  あお: {
+    eng: "blue",
+    content: "🔵🎨"
+  },
+  いえ: {
+    eng: "house",
+    content: "🏠"
+  },
+  とけい: {
+    eng: "watch/clock",
+    content: "⌚🕰️"
+  },
+  すし: {
+    eng: "sushi",
+    content: "🍣"
+  },
+  はい: {
+    eng: "yes",
+    content: "👍"
+  },
+  いいえ: {
+    eng: "no",
+    content: "👎"
+  },
+  しお: {
+    eng: "salt",
+    content: "🧂"
+  }
+};
 
 export default words;
