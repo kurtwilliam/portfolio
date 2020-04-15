212481e51dda97ae9e06b7b79c6e4e1ff5e9410c
dinkiebinkie <b@binkie.town>
2020-04-15 19:22:03 -0400

    improving file sorting

--- frontend/src/Components/Body/GameOfLife/Shapes/index.js
+++ frontend/src/Components/Body/GameOfLife/Shapes/index.js
@@ -1,6 +1,41 @@
+function numericalSortObjectOfObjects(data, attr) {
+  var arr = [];
+  for (var prop in data) {
+    if (data.hasOwnProperty(prop)) {
+      var obj = {};
+      obj[prop] = data[prop];
+      obj.tempSortName =
+        typeof data[prop][attr] === "number"
+          ? data[prop][attr]
+          : data[prop][attr].toLowerCase();
+      arr.push(obj);
+    }
+  }
+
+  arr.sort(function(a, b) {
+    var at = a.tempSortName,
+      bt = b.tempSortName;
+    return at > bt ? 1 : at < bt ? -1 : 0;
+  });
+
+  var result = [];
+  for (var i = 0, l = arr.length; i < l; i++) {
+    var obj = arr[i];
+    delete obj.tempSortName;
+    for (var prop in obj) {
+      if (obj.hasOwnProperty(prop)) {
+        var id = prop;
+      }
+    }
+    var item = obj[id];
+    result.push(item);
+  }
+  return result;
+}
+
 // Aggregate all shapes into an object
 const getFiles = () => {
-  var context = require.context(".", true, /\.js$/);
+  let context = require.context(".", true, /\.js$/);
   const files = {};
 
   context.keys().forEach(function(key) {
@@ -12,9 +47,13 @@ const getFiles = () => {
     }
 
     files[newKey[0]] = context(key).default;
+    files[newKey[0]].dimensionSize =
+      files[newKey[0]].config[0].length + files[newKey[0]].config.length;
   });
 
-  return files;
+  const sortedFileObj = numericalSortObjectOfObjects(files, "dimensionSize");
+
+  return sortedFileObj;
 };
 
 export default getFiles();
