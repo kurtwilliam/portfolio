846e799d176826a496266c0719706ee9f9f8e4a9
dinkiebinkie <b@binkie.town>
2020-05-14 13:57:04 -0400

    adding patterns

---
+++ frontend/src/Components/Body/GameOfLife/patterns/CoeShip.js
@@ -0,0 +1,17 @@
+export default {
+  id: 8122,
+  name: "Coe Ship",
+  type: "Space Ship",
+  source: "https://www.conwaylife.com/wiki/Coe_ship",
+  config: [
+    [false, false, false, false, true, true, true, true, true, true],
+    [false, false, true, true, false, false, false, false, false, true],
+    [true, true, false, true, false, false, false, false, false, true],
+    [false, false, false, false, true, false, false, false, true],
+    [false, false, false, false, false, false, true],
+    [false, false, false, false, false, false, true, true],
+    [false, false, false, false, false, true, true, true, true],
+    [false, false, false, false, false, true, true, false, true, true],
+    [false, false, false, false, false, false, false, true, true]
+  ]
+};
